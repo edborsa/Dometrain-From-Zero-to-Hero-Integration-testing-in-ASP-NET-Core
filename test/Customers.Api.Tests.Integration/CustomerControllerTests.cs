5b60d9ed60cc078ffcf2329534f87dc9e4dae9f4
Eduardo Borsa <eborsa@loomissayles.com>
2024-02-05 15:52:04 -0300

    Watched Testing status codes
    Signed-off-by: Eduardo Borsa <eborsa@loomissayles.com>

--- test/Customers.Api.Tests.Integration/CustomerControllerTests.cs
+++ test/Customers.Api.Tests.Integration/CustomerControllerTests.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace Customers.Api.Tests.Integration;
@@ -16,6 +17,6 @@ public class CustomerControllerTests : IClassFixture<WebApplicationFactory<IAPIM
     public async Task Customer_Returns_NotFound_When_Customer_Does_NOT_Exists()
     {
         var response = await _httpClient.GetAsync($"customers/{Guid.NewGuid()}");
-        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
     }
 }
