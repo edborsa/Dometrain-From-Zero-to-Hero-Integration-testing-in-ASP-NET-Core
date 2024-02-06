50d49d20fc28f0c858aa67be451e9698c17531a4
Eduardo Borsa <eborsa@loomissayles.com>
2024-02-06 08:51:36 -0300

    Watch Creating realistic test data
    Signed-off-by: Eduardo Borsa <eborsa@loomissayles.com>

--- test/Customers.Api.Tests.Integration/CustomerControllerTests.cs
+++ test/Customers.Api.Tests.Integration/CustomerControllerTests.cs
@@ -1,5 +1,9 @@
 using System.Net;
 using System.Net.Http.Json;
+using Bogus;
+using Customers.Api.Contracts.Requests;
+using Customers.Api.Contracts.Responses;
+using Customers.Api.Domain.Common;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Testing;
@@ -9,6 +13,11 @@ namespace Customers.Api.Tests.Integration;
 public class CustomerControllerTests : IClassFixture<WebApplicationFactory<IAPIMarker>>
 {
     private readonly HttpClient _httpClient;
+    private readonly Faker<CustomerRequest> _customerGenerator = new Faker<CustomerRequest>()
+        .RuleFor(x => x.FullName, faker => faker.Person.FullName)
+        .RuleFor(x => x.Email, faker => faker.Person.Email)
+        .RuleFor(x => x.GitHubUsername, faker => "nickchapsas")
+        .RuleFor(x => x.DateOfBirth, faker => faker.Person.DateOfBirth.Date);
 
     public CustomerControllerTests(WebApplicationFactory<IAPIMarker> appFactory)
     {
@@ -23,6 +32,21 @@ public class CustomerControllerTests : IClassFixture<WebApplicationFactory<IAPIM
         var problem = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
         problem!.Title.Should().Be("Not Found");
         problem!.Status.Should().Be(404);
+
+    }
+
+    [Fact]
+    public async Task Create_ReturnsCreated_WhenCustomerIsCreated()
+    {
+        var customer = _customerGenerator.Generate();
+        
+        var response = await _httpClient.PostAsJsonAsync("customers", customer);
+
+        var customerResponse = await response.Content.ReadFromJsonAsync<CustomerResponse>();
+        customerResponse.Should().BeEquivalentTo(customer);
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+
+
 
     }
 }
