7f7f72ea2e423c12d1e6dfa45f2242e4bd03707f
Eduardo Borsa <eborsa@loomissayles.com>
2024-02-15 13:40:55 -0300

    Testing: Updating a resource -> Delete_ReturnsOK_WhenCustomerExists OK
    Signed-off-by: Eduardo Borsa <eborsa@loomissayles.com>

---
+++ test/Customers.Api.Tests.Integration/CustomerController/DeleteCustomerControllerTests.cs
@@ -0,0 +1,49 @@
+using System.Net;
+using System.Net.Http.Json;
+using Bogus;
+using Customers.Api.Contracts.Requests;
+using Customers.Api.Contracts.Responses;
+using FluentAssertions;
+
+namespace Customers.Api.Tests.Integration.CustomerController;
+
+public class DeleteCustomerControllerTests : IClassFixture<CustomerApiFactory>
+{
+    private readonly HttpClient _client;
+
+    private readonly Faker<CustomerRequest> _customerGenerator = new Faker<CustomerRequest>()
+        .RuleFor(x => x.Email, faker => faker.Person.Email)
+        .RuleFor(x => x.FullName, faker => faker.Person.FullName)
+        .RuleFor(x => x.GitHubUsername, CustomerApiFactory.ValidGithubUser)
+        .RuleFor(x => x.DateOfBirth, faker => faker.Person.DateOfBirth.Date);
+
+    public DeleteCustomerControllerTests(CustomerApiFactory apiFactory)
+    {
+        _client = apiFactory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Delete_ReturnsOK_WhenCustomerExists()
+    {
+        // Arrange
+        var customer = _customerGenerator.Generate();
+        var response = await _client.PostAsJsonAsync("customers", customer);
+        var createCustomerResponse = await response.Content.ReadFromJsonAsync<CustomerResponse>();
+        // Act
+        var deleteResponse = await _client.DeleteAsync($"customers/{createCustomerResponse!.Id}");
+        // Assert
+        deleteResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    // [Fact]
+    // public async Task Get_ReturnsNotFound_WhenCustomerDoesNotExists()
+    // {
+    //     // Arrange
+    //     var customer = _customerGenerator.Generate();
+    //     // Act
+    //     var getResponse = await _client.GetAsync($"customers/{Guid.NewGuid()}");
+    //     // Assert
+    //     var getCustomerResponse = await getResponse.Content.ReadFromJsonAsync<CustomerResponse>();
+    //     getResponse.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    // }
+}
